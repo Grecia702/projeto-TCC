0eb7182f2912df98ea2f3251488b0b1a925b99ae
Gabriel Grecia <gabriel.lena81@gmail.com>
2025-05-03 07:23:13 -0400

    fix deploy

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const authRoutes = require('./routes/authRoutes')
-const userRoutes = require('./routes/userRoutes')
-const accountRoutes = require('./routes/accountRoutes')
-const transactionRoutes = require('./routes/transactionRoutes')
-const logger = require('./utils/loggerConfig')
+const authRoutes = require(path.join(__dirname, 'routes', 'authRoutes'));
+const userRoutes = require(path.join(__dirname, 'routes', 'userRoutes'));
+const accountRoutes = require(path.join(__dirname, 'routes', 'accountRoutes'));
+const transactionRoutes = require(path.join(__dirname, 'routes', 'transactionRoutes'));
+const logger = require(path.join(__dirname, 'utils', 'loggerConfig'));
 const cookieParser = require('cookie-parser');
 const { RateLimiterMemory } = require('rate-limiter-flexible');
-const { iniciarCron } = require('./utils/transacoesRecorrentes');
+const { iniciarCron } = require(path.join(__dirname, 'utils', 'transacoesRecorrentes'));
 
 iniciarCron();
 
@@ -31,13 +31,11 @@ app.use(cors({
         'http://192.168.100.211:8082',
         'http://127.0.0.1:8081',
         'http://127.0.0.1:8082'
-
     ],
     credentials: true,
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
 
 app.use((req, res, next) => {
     rateLimiter.consume(req.ip)
@@ -56,10 +54,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use("/api/users", userRoutes)
-app.use("/api/auth", authRoutes)
-app.use("/api/profile/account", accountRoutes)
-app.use("/api/profile/transaction", transactionRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/profile/account", accountRoutes);
+app.use("/api/profile/transaction", transactionRoutes);
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', 'index.html'));
