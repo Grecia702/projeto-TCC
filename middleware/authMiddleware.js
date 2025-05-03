38ab387e306a951baac5fcd4ab18e6eaccf537eb
Gabriel Grecia <gabriel.lena81@gmail.com>
2025-05-03 08:19:47 -0400

    fix no. 13

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
-const { verifyAccessToken } = require('../utils/tokenUtils');
+const { verifyAccessToken } = require('../Utils/tokenUtils');
 
 module.exports = (req, res, next) => {
     let token = req.cookies.accessToken;
